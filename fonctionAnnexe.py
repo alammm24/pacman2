a2a2cee13750627e66c5ca7f4827b918d625a266
dan-serraf <55733346+dan-serraf@users.noreply.github.com>
2019-12-22 15:58:29 +0100

    Update fonctionAnnexe.py

--- fonctionAnnexe.py
+++ fonctionAnnexe.py
@@ -3,6 +3,7 @@ from fonction_annexe import *
 from time import sleep
 from time import time
 from random import randint
+import pygame
 
 def teleportation(personnage,largeur_terrain,longeur_terrain):
     """ Cette fonction permet de passer d'un cote du mur et de reapparaitre de l'autre """
@@ -18,57 +19,89 @@ def teleportation(personnage,largeur_terrain,longeur_terrain):
             
     elif personnage["y"] > longeur_terrain:
             personnage["y"] = 0
+            
     
-def se_deplacer(personnage) :
-    """ Cette fonction ajoute une nouvelle tête et supprime le corp pour donner l'impression de déplacement """
+def deplace_personnages(pacMan,fantomes,murs):
+    deplace_pac_man(pacMan,murs)
+    deplace_fantomes(fantomes,murs)
+    
+def deplace_pac_man(personnage,murs) :
+   
+    if valide_deplacement(personnage,murs) :
+        deplace_personnage(personnage)
+
+def deplace_fantomes(fantomes,murs) :
+    for fantome in fantomes:
+        deplace_fantome(fantome,murs)
+        
+def deplace_fantome(personnage,murs) :
+    
+    if valide_deplacement(personnage,murs) :
+        deplace_personnage(personnage)
+    else :
+        while not valide_deplacement(personnage,murs) :
+            change_direction_fantome(fantome)
+            deplace_personnage(personnage)
+
+def deplace_personnage(personnage) :
     personnage["x"]+= personnage["direction"][0]
     personnage["y"]+= personnage["direction"][1]
-
-def deplace_personnage(pacMan,fantomes):
-    se_deplacer(pacMan)
     
     
-    for i in range(0,len(fantomes)):
-        se_deplacer(fantomes[i])
+def valide_deplacement(personnage,murs) :
+    for i in murs :
+        if (i[0] == personnage["x"]+ personnage["direction"][0]) and  (i[1] == personnage["y"]+ personnage["direction"][1]) :
+            return False
+    
+    return True
+    
 
-def change_direction(pacMan, touche):
+def change_direction_pac_man(pacMan, touche):
     
     if touche == 'Up':  # flèche haut pressée
         pacMan["image"] = './image/pacman_haut.png'
-        return [0, -1]
+        pacMan["direction"]=[0, -10]
         
     elif touche == 'Down': # flèche bas pressée
         pacMan["image"] = './image/pacman_bas.png'
-        return [0, 1]
+        pacMan["direction"]= [0, 10]
         
     elif touche == 'Left': # flèche gauche pressée
         pacMan["image"] = './image/pacman_gauche.png'
-        return [-1, 0]
+        pacMan["direction"]= [-10, 0]
         
     elif touche == 'Right':  # flèche droite pressée
         pacMan["image"] = './image/pacman_droite.png'
-        return[1, 0]
+        pacMan["direction"]=[10, 0]
         
     else :                 #afin de pas provoque une erreur si on appuie pas sur autre touche
-        return pacMan["direction"]
+        pacMan["direction"]= pacMan["direction"]
+        
+    
 
 def change_direction_fantome(fantome):
     rand = randint(0,5)
-    direction = [[0, -1],[0, 1],[-1, 0],[1, 0]]
+    direction = [[0, -10],[0, 10],[-10, 0],[10, 0]]
     fantome["direction"] = direction[rand]
-    return fantome
 
-def initialise_fantomes():
-    ''' '''
-    return [           { "x":300 , "y":300 , "image" : './image/pacman_bleu.png' , "vitesse" :10 ,"vulnerable" : False, "direction" : [1,0]},
-                { "x":270 , "y":300 , "image" : './image/pacman_jaune.png' , "vitesse" :10,"vulnerable" : False, "direction" : [1,0]},
-                { "x":330 , "y":300 , "image" : './image/pacman_rose.png' , "vitesse" :10,"vulnerable" : False, "direction" : [1,0]},
-                { "x":300 , "y":270 , "image" : './image/pacman_rouge.png' , "vitesse" :10,"vulnerable" : False, "direction" : [1,0]}]
+def initialise_fantomes(fantomes):
+    fantomes.append({ "x":300 , "y":300 , "image" : './image/pacman_bleu.png' , "vitesse" :10 ,"vulnerable" : False, "direction" : [10,0]})
+    fantomes.append({ "x":270 , "y":300 , "image" : './image/pacman_jaune.png' , "vitesse" :10,"vulnerable" : False, "direction" : [10,0]})
+    fantomes.append({ "x":330 , "y":300 , "image" : './image/pacman_rose.png' , "vitesse" :10,"vulnerable" : False, "direction" : [10,0]})
+    fantomes.append({ "x":300 , "y":270 , "image" : './image/pacman_rouge.png' , "vitesse" :10,"vulnerable" : False, "direction" : [10,0]})    
+
+def reinitialise_fantomes(fantomes):
+    fantomes[0] = { "x":300 , "y":300 , "image" : './image/pacman_bleu.png' , "vitesse" :10 ,"vulnerable" : False, "direction" : [10,0]}
+    fantomes[1] = { "x":270 , "y":300 , "image" : './image/pacman_jaune.png' , "vitesse" :10,"vulnerable" : False, "direction" : [10,0]}
+    fantomes[2] = { "x":330 , "y":300 , "image" : './image/pacman_rose.png' , "vitesse" :10,"vulnerable" : False, "direction" : [10,0]}
+    fantomes[3] =  { "x":300 , "y":270 , "image" : './image/pacman_rouge.png' , "vitesse" :10,"vulnerable" : False, "direction" : [10,0]}
+
+
     
 
 def initialise_pacMan():
     ''' '''
-    return { "x":10 , "y":10 , "image" : './image/pacman_droite.png' , "vie": 3, "vitesse" :1000, "score":0 , "direction" : [1,0]}
+    return { "x":10 , "y":10 , "image" : './image/pacman_droite.png' , "vie": 3, "vitesse" :100, "score":0 , "direction" : [10,0]}
 
 def initialise_coordonne_pacMan(pacMan):
     ''' '''
@@ -82,48 +115,44 @@ def initialise_coordonne_fantome(fantome):
     fantome["x"] = 100
     fantome["y"] = 100
     
-    return fantome
 
 
 def initialiseJeu(pacMan,fantomes,points,superPoints,cerises):
     initialise_points()
     initialise_superPoints()
     initialise_cerises()
-    initialise_fantomes()
+    initialise_fantomes(fantomes)
     initialise_pacMan()
-    
+
+
         
 def collision(pacMan,fantomes,points,superPoints,cerises):
     
-    for i in range(0,len(fantomes)):
-        collision_pacMan_fantome(pacMan, fantomes[i])
+    for fantome in fantomes:
+        collision_pacMan_fantome(pacMan, fantome,fantomes)
     
-    for i in range(0,len(points)):
-        collision_pacMan_point(pacMan, points[i])
+    for point in points:
+        collision_pacMan_point(pacMan, point)
     
-    for i in range(0,len(cerises)):
-        collision_pacMan_cerise(pacMan, cerises[i])
+    for cerise in cerises:
+        collision_pacMan_cerise(pacMan, cerise)
     
-    for i in range(0,len(superPoints)):    
-        collision_pacMan_superPoint(pacMan, superPoints[i],fantome)
+    for superPoint in superPoints:    
+        collision_pacMan_superPoint(pacMan, superPoint,fantomes)
         
         
-def collision_pacMan_fantome(pacMan, fantome):
+def collision_pacMan_fantome(pacMan, fantome,fantomes):
     
    
     if (fantome["vulnerable"] == False) and (fantome["x"] == pacMan["x"]) and (fantome["y"] == pacMan["y"] ) :
         pacMan["vie"] += -1 
-        pacMan = initialise_coordonne_pacMan(pacMan)
-        initialise_fantomes() # on initialises tous les fantomes
+        initialise_coordonne_pacMan(pacMan)
+        reinitialise_fantomes(fantomes) # on initialises tous les fantomes
         
     if (fantome["vulnerable"] == True) and ( fantome["x"] == pacMan["x"] and fantome["y"] == pacMan["y"] ) :
-        fantome = initialise_coordonne_fantome(fantome)
+        initialise_coordonne_fantome(fantome)
 
-def collision_fantome_mur(pacMan, mur):
-    if (fantome["x"] == mur["x"]) and (fantome["y"] == mur["y"] ) :
-        return True
-    return False
-        
+
     
 
 def collision_pacMan_point(pacMan, point):
@@ -184,21 +213,33 @@ def gameOver(pacMan):
         return True
     return False
     
-def case_vers_pixel(x, y,terrain):
-    return x * terrain["TAILLE_CASE_LARGEUR"], y * terrain["TAILLE_CASE_HAUTEUR"]
-
-def pixel_vers_case(x, y) :
-     return x // terrain["TAILLE_CASE_LARGEUR"], y //terrain["TAILLE_CASE_HAUTEUR"]
+# def change_direction_fantomes(fantomes,murs,pacMan) :
+#     for i in range(0,len(fantomes)):
+#         for j in range(0,len(murs)):
+#             if collision_pacMan_fantome(pacMan, fantome) :
+#                 change_direction_fantome(fantomes[i])# direction
     
-def pixel_vers_case_vers_pixel(x,y) :
-    x,y = pixel_vers_case(x,y)
-    x,y = case_vers_pixel(x,y)
-    return x,y 
+# def collision_fantome_mur(pacMan, mur):
+#     if (fantome["x"] == mur["x"]) and (fantome["y"] == mur["y"] ) :
+#         return True
+#     return False
+        
     
-def case_vers_pixel_vers_case(x,y) :
-    x,y = case_vers_pixel(x,y)
-    x,y = pixel_vers_case(x,y)
-    return x,y 
+# def case_vers_pixel(x, y,terrain):
+#     return x * terrain["TAILLE_CASE_LARGEUR"], y * terrain["TAILLE_CASE_HAUTEUR"]
+# 
+# def pixel_vers_case(x, y) :
+#      return x // terrain["TAILLE_CASE_LARGEUR"], y //terrain["TAILLE_CASE_HAUTEUR"]
+#     
+# def pixel_vers_case_vers_pixel(x,y) :
+#     x,y = pixel_vers_case(x,y)
+#     x,y = case_vers_pixel(x,y)
+#     return x,y 
+#     
+# def case_vers_pixel_vers_case(x,y) :
+#     x,y = case_vers_pixel(x,y)
+#     x,y = pixel_vers_case(x,y)
+#     return x,y 
 
 
 
